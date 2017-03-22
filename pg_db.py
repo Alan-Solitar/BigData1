00679e091b961df6d96de93dce23235c09cf6685
Alan-Solitar <alsolita1@gmail.com>
2017-03-21 20:38:19 -0400

    Add files via upload

--- pg_db.py
+++ pg_db.py
@@ -9,7 +9,9 @@ class PatientDataManager():
 
     def __init__(self,exists=False):
         self.base_file_name = 'patients.csv'
+        self.base_file_name_diag = 'ROSMAP_RNASeq_entrez.csv'
         self.table_name = 'patients_info'
+        self.table_name_diag = 'diagnosis_info'
         self.db_name = 'patients_db'
         self.user,self.pw = self.get_info()
         self.fields = self.parse_base_file(self.base_file_name)
@@ -26,21 +28,24 @@ class PatientDataManager():
         #This is needed to create the database
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
         cur = conn.cursor()
-        db_create = 'CREATE DATABASE {}'.format(self.db_name)
-        cur.execute(db_create)
-        conn.commit()
+        try:
+            db_create = 'CREATE DATABASE {}'.format(self.db_name)
+            cur.execute(db_create)
+            conn.commit()
+        except:
+            'DB exists'
         cur.close()
         conn.close()
-    def create_table(self):
+    def create_tables(self):
         conn = psycopg2.connect("dbname='{}' user='{}' host='localhost' password='{}'".format(self.db_name,self.user,self.pw))
         cur = conn.cursor()
-        query = 'CREATE TABLE {} ({} TEXT PRIMARY KEY, {} TEXT, {} TEXT, {} TEXT)'.format(self.table_name,self.fields[0],self.fields[1],self.fields[2],self.fields[3])
-        print(query)
-        cur.execute(query)
+        query1 = 'CREATE TABLE IF NOT EXISTS {} ({} TEXT PRIMARY KEY, {} TEXT, {} TEXT, {} TEXT)'.format(self.table_name,self.fields[0],self.fields[1],self.fields[2],self.fields[3])
+        query2= 'CREATE TABLE IF NOT EXISTS {} ({} TEXT PRIMARY KEY, DIAGNOSIS TEXT)'.format(self.table_name_diag,self.fields[0])
+        cur.execute(query1)
+        cur.execute(query2)
         conn.commit()
         cur.close()
         conn.close()
-
     def populate_db(self,file_name):
         df = pd.read_csv(file_name)
         df.age = df.age.astype(str)
@@ -55,10 +60,22 @@ class PatientDataManager():
         conn.commit()
         cur.close()
         conn.close()
+    def populate_diagnosis_table(self,file_name):
+        df = pd.read_csv(file_name)
+        df = df[['PATIENT_ID','DIAGNOSIS']]
+        tups = df.to_records(index=False)
+        
+        conn = psycopg2.connect("dbname='{}' user='postgres' host='localhost' password='pgpwd'".format(self.db_name))
+        cur = conn.cursor()
+        query = "INSERT INTO {} ({},{}) VALUES(%s,%s)".format(self.table_name_diag,'PATIENT_ID','DIAGNOSIS')
+        cur.executemany(query,tups)
+        conn.commit()
+        cur.close()
+        conn.close()
     def get_patient_info(self,patient_id):
         conn = psycopg2.connect("dbname='{}' user='postgres' host='localhost' password='pgpwd'".format(self.db_name))
         cur = conn.cursor()
-        query = "SELECT * FROM {} WHERE {} = '{}'".format(self.table_name,self.fields[0],patient_id)
+        query = "SELECT {}.*, {}.DIAGNOSIS FROM {} INNER JOIN {} ON {}.patient_id = {}.patient_id WHERE {}.{} = '{}'".format(self.table_name,self.table_name_diag,self.table_name,self.table_name_diag,self.table_name,self.table_name_diag,self.table_name,self.fields[0],patient_id)
         cur.execute(query)
         info = (cur.fetchone())
         cur.close()
@@ -71,13 +88,15 @@ class PatientDataManager():
         return ROSMAP_Collection.find_one({"PATIENT_ID" : patient_id},{'DIAGNOSIS':1,'_id':0})['DIAGNOSIS']
     def initiate(self):
         self.create_db()
-        self.create_table()
+        self.create_tables()
+        self.populate_diagnosis_table(self.base_file_name_diag)
         self.populate_db(self.base_file_name)
     def query_for_patient_info(self):
         patient_id = input('patient_id: ')
         info = self.get_patient_info(patient_id)
-        diagnosis  = self.get_diagnosis(patient_id)
-        print (' '.join([val for val in info]),diagnosis)
+        #diagnosis  = self.get_diagnosis(patient_id)
+        if info != None:
+            print (' '.join([val for val in info]))
 
-pdm = PatientDataManager(True)
+pdm = PatientDataManager(False)
 pdm.query_for_patient_info()
